776622327cc6ce6c5d948310309e2b1e93636b73
Alvaro Martinez <alvaromfdv@gmail.com>
2024-07-25 19:07:38 +0200

    Added Responses + styling

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,5 +1,12 @@
 //import { IconButton, Stack } from "@mui/material"
-import { Card, Typography } from "@mui/material";
+import {
+  Card,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useQuestionsStore } from "./store/questions";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
@@ -8,12 +15,25 @@ import { type Question as QuestionType } from "./types";
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined" sx={{bgcolor:'#282C34',p:2, textAlign:'left'}}>
+    <Card
+      variant="outlined"
+      sx={{ bgcolor: "#282C34", p: 2, textAlign: "left" }}
+    >
       <Typography variant="h5">{info.question}</Typography>
 
       <SyntaxHighlighter language="javascript" style={atomOneDarkReasonable}>
         {info.code}
       </SyntaxHighlighter>
+
+      <List sx={{ bgcolor: "#3c4047" }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton >
+              <ListItemText primary={answer} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   );
 };
