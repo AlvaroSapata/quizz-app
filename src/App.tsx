0fb8d0d624fdc427bd82449db78839a888b04ff4
Alvaro Martinez <alvaromfdv@gmail.com>
2024-07-25 18:56:44 +0200

    MUI dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { Container, Stack, Typography } from "@mui/material";
 import { JavaScriptLogo } from "./JavaScriptLogo";
 import { Start } from "./Start";
 import { useQuestionsStore } from "./store/questions";
+import { Game } from "./Game";
 
 function App() {
   const questions = useQuestionsStore((state) => state.questions);
   console.log(questions, questions.length);
-  
+
   return (
     <main>
       <Container maxWidth="sm">
@@ -21,7 +22,7 @@ function App() {
           <Typography variant="h2">JavaScript Quizz</Typography>
         </Stack>
         {questions.length === 0 && <Start />}
-        {questions.length > 0 && <h1>Juego</h1>}
+        {questions.length > 0 && <Game />}
       </Container>
     </main>
   );
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,19 @@
 //import { IconButton, Stack } from "@mui/material"
 import { Card, Typography } from "@mui/material";
 import { useQuestionsStore } from "./store/questions";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
 import { type Question as QuestionType } from "./types";
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined">
+    <Card variant="outlined" sx={{bgcolor:'#282C34',p:2, textAlign:'left'}}>
       <Typography variant="h5">{info.question}</Typography>
+
+      <SyntaxHighlighter language="javascript" style={atomOneDarkReasonable}>
+        {info.code}
+      </SyntaxHighlighter>
     </Card>
   );
 };
