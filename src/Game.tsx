830520ddd24477f1a42dd8fb7ab2c9a7e39200df
Alvaro Martinez <alvaromfdv@gmail.com>
2024-07-26 10:48:56 +0200

    Show correct answer + bck color

--- src/Game.tsx
+++ src/Game.tsx
@@ -13,11 +13,31 @@ import { atomOneDarkReasonable } from "react-syntax-highlighter/dist/esm/styles/
 
 import { type Question as QuestionType } from "./types";
 
+const getBackgroundColor = (info: QuestionType, index: number) => {
+    const { userSelectedAnswer, correctAnswer } = info
+    // usuario no ha seleccionado nada todavía
+    if (userSelectedAnswer == null) return 'transparent'
+    // si ya selecciono pero la solución es incorrecta
+    if (index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+    // si esta es la solución correcta
+    if (index === correctAnswer) return 'green'
+    // si esta es la selección del usuario pero no es correcta
+    if (index === userSelectedAnswer) return 'red'
+    // si no es ninguna de las anteriores
+    return 'transparent'
+  }
+
 const Question = ({ info }: { info: QuestionType }) => {
+    const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+  
+    const createHandleClick = (answerIndex: number) => () => {
+      selectAnswer(info.id, answerIndex)
+    }
+
   return (
     <Card
       variant="outlined"
-      sx={{ bgcolor: "#282C34", p: 2, textAlign: "left" }}
+      sx={{ bgcolor: "#282C34", p: 2, textAlign: "left", marginTop: 4 }}
     >
       <Typography variant="h5">{info.question}</Typography>
 
@@ -25,11 +45,17 @@ const Question = ({ info }: { info: QuestionType }) => {
         {info.code}
       </SyntaxHighlighter>
 
-      <List sx={{ bgcolor: "#3c4047" }} disablePadding>
+      <List sx={{ bgcolor: '#3c4047' }} disablePadding>
         {info.answers.map((answer, index) => (
-          <ListItem key={index} disablePadding>
-            <ListItemButton >
-              <ListItemText primary={answer} />
+          <ListItem key={index} disablePadding divider>
+            <ListItemButton
+              disabled={info.userSelectedAnswer != null}
+              onClick={createHandleClick(index)}
+              sx={{
+                backgroundColor: getBackgroundColor(info, index)
+              }}
+            >
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
             </ListItemButton>
           </ListItem>
         ))}
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -5,19 +5,40 @@ interface State {
   questions: Question[];
   currentQuestion: number;
   fetchQuestions: (limit: number) => Promise<void>;
+  selectAnswer: (questionId: number, answerIndex: number) => void;
 }
 
-export const useQuestionsStore = create<State>((set,get) => {
+export const useQuestionsStore = create<State>((set, get) => {
   return {
     questions: [],
     currentQuestion: 0, // posicion array Questions
 
     fetchQuestions: async (limit: number) => {
-      const res = await fetch('http://localhost:5174/data.json');
+      const res = await fetch("http://localhost:5174/data.json");
       const json = await res.json();
 
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
       set({ questions });
+    },
+    
+    selectAnswer: (questionId: number, answerIndex: number) => {
+      const {questions} = get();
+      // Usar el structuredClone para clonar el objeto
+      const newQuestions = structuredClone(questions);
+      // Encontrar el indice de la pregunta
+      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
+      // Obtener la informacion de la pregunta
+      const questionInfo = newQuestions[questionIndex];
+      // Verificar si la respuesta es correcta
+      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
+      // Actualizar la informacion en la copia de la pregunta
+      newQuestions[questionIndex] = {
+        ...questionInfo,
+        userSelectedAnswer: answerIndex,
+        isCorrectUserAnswer,
+      };
+      // Actualizar el estado
+      set({ questions: newQuestions });
     },
   };
 });
